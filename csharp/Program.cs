aeb0e43d7f388617b402b4ab0b94b9a24095cfc4
BulgPride <Dekryptor@users.noreply.github.com>
2023-05-30 05:37:03 +0300

    Create Program.cs

---
+++ csharp/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Timers;
+//using System.Threading;
+using System.Threading.Tasks;
+					
+public class Program
+{
+	public static void Main()
+	{
+        var timer = new Timer(2000);
+		timer.Elapsed += async (sender, eventArgs) =>
+		{
+			//await Task.Run(() => Console.WriteLine($"Elapsed event at {eventArgs.SignalTime:G}"));
+			await Task.Run(() => OnEventExecution(sender, eventArgs));
+			//throw new Exception();
+		};
+		//timer.AutoReset = false;
+		timer.Start();
+ 		
+		Console.ReadKey();
+	}
+	
+	public static void OnEventExecution(object sender, ElapsedEventArgs eventArgs)
+    {
+        Console.WriteLine($"Elapsed event at {eventArgs.SignalTime:G}");
+    }
+}
